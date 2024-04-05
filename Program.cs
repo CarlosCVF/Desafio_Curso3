5dd7a84228a72e7fd63bb9728f3f8c92e6fc3f90
CarlosCVF <carloscvf03@gmail.com>
2024-04-05 13:44:09 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//Escrever um programa que faça uam requisição para a API de games CheapShark e mostre na tela a lista de promoções cadastrada na ferramenta (Você pode utilizar o endpoint: https://www.cheapshark.com/api/1.0/deals).
+using (HttpClient client = new HttpClient())
+{
+    
+}
