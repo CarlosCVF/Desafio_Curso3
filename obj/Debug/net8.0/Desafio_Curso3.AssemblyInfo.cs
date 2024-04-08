6013511451856f85ed719fdc65ea4b11fbe1e361
CarlosCVF <carloscvf03@gmail.com>
2024-04-08 10:57:33 -0300

    commit

--- obj/Debug/net8.0/Desafio_Curso3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Desafio_Curso3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio_Curso3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc80f1c09732a4c3c9a571a4569ae06bc98ddfe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf6c3a33756adad5ebcf0b88117c96871d89cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio_Curso3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio_Curso3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
